a21c333c97fbe45e0279bb3eb2c3edf3d6c37afe
khaismile1997 <khaismile1997@gmail.com>
2022-06-15 13:29:42 +0700

    setting bootstrap & jquery in rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,17 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "bootstrap"
+import "../stylesheets/application"
+
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+import '@doabit/semantic-ui-sass'
+// import 'stylesheets/semantic-ui.scss'
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
